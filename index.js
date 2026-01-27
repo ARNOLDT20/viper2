330d477b1145d8d908c0bd2771d88e60c7747593
ARNOLD <atarimo117@gmail.com>
2026-01-27 09:53:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@
 // github @ARNOLDT20
 // WhatsApp @255627417402
 // telegram t.me/starboyTechInfo 
-for (let membre of membres) {
     // Website blazetech-website.vercel.com
     // Enjoy Movies update starboy-movies-library.vercel.app
     // WE AVAILABLE ALL TIME TO RECEIVE YOU REQUEST FOR ANY DEV OR UPCOMING DEV IN WHATSAPP BOTS
@@ -22,8 +21,8 @@ for (let membre of membres) {
     var __createBinding = (this && this.__createBinding) || (Object.create ? (function (o, m, k, k2) {
         if (k2 === undefined) k2 = k;
         var desc = Object.getOwnPropertyDescriptor(m, k);
-        if (group.action == 'add' && (await recupevents(group.id, "welcome") == 'on')) {
-            let msg = `*viper xmd*\n\nWelcome @${membre.split("@")[0]} to our group.\n\nPlease read the group description to avoid removals.`;
+        if (!desc || (!('get' in desc) && (desc.writable || desc.configurable))) {
+            desc = { enumerable: true, get: function () { return m[k]; } };
         }
         Object.defineProperty(o, k2, desc);
     }) : (function (o, m, k, k2) {
